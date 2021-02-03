966afad732d02ea8b4e1ec25205ef1aad1bf7818
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-03 22:38:47 +0000

    Second read of 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
-Binit main(void)
+#include <stdio.h>
+
+init main(void)
 {
-puts("Programming is like building a multilingual puzzle")
-  (return 0;)
+  puts("Programming is like building a multilingual puzzle");
+  (return 0);
 }
