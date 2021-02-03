317a6e884348cd43d4fb6fdc43b5c5dd0685e3b4
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-03 23:19:15 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void)
-/** */
+/** Descriptor out of necessity*/
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("Programming is like building a multilingual puzzle");
+getchar()
 return (0);
 }
