f95c2094427d9e4eb6e452b22ea454d65859a056
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-04 00:01:15 +0000

    6 init

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main- functions start
+*
+* Return-function end, reset to main
+*/
+
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+char charType;
+
+printf("Size of int: %zu bytes\n");
+printf("Size of float: %zu bytes\n");
+printf("Size of double: %zu bytes\n");
+printf("Size of char: %zu byte\n");
+
+return 0;
+}
