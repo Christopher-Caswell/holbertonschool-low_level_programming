db11666292a8f30f7dc398755f7d00cc1b30cd7e
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-05 15:53:56 +0000

    Init all files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,17 +9,19 @@
 int main(void)
 {
 
-int a1;
-float a2;
-double a3;
-char a4;
-long long a5;
+int g1;
+float g2;
+double g3;
+char g4;
+long long g5;
+long g6;
 
-printf("size of long long: %lu bytes\n", sizeof(a5));
-printf("Size of int: %lu bytes\n", sizeof(a1));
-printf("Size of float: %lu bytes\n", sizeof(a2));
-printf("Size of double: %lu bytes\n", sizeof(a3));
-printf("Size of char: %lu byte\n", sizeof(a4));
+ printf("size of int: %lu bytes\n", size(g1));
+printf("size of float: %lu bytes\n", sizeof(g2));
+printf("Size of double: %lu bytes\n", sizeof(g3));
+printf("Size of char: %lu bytes\n", sizeof(g4));
+printf("Size of long long: %lu bytes\n", sizeof(g5));
+printf("Size of long: %lu byte\n", sizeof(g6));
 
 return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,6 +13,7 @@ int main(void)
 {
 int n;
 int l4;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
