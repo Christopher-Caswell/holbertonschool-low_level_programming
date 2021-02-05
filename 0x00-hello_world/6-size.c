e803df053ef6ffed66fed72558a6a2ecf55a4f16
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-05 16:52:22 +0000

    6 init3.7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,8 @@ int main(void)
 
 printf("size of char: %zu bytes\n", sizeof(char));
 printf("size of int: %zu bytes\n", sizeof(int));
-printf("Size of long: %zu bytes\n", sizeof(long int));
-printf("Size of long long: %zu bytes\n", sizeof(long long int));
+printf("Size of long int: %zu bytes\n", sizeof(long));
+printf("Size of long long int: %zu bytes\n", sizeof(long long));
 printf("Size of float: %zu byte\n", sizeof(float));
 
 return (0);
