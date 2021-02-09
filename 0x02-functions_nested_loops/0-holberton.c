1e3d718335fbb3dc653043ab4f754a9499a0300e
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-09 16:10:45 +0000

    Init repo

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,46 @@
-#include <holberton.h>
+#include "holberton.h"
 
 /**
 * main - determining the functions of the code
-* holberton.h will define the function of _putchar
-* Return - 0, no mas loop
+* holberton.h will define the function of _putchar, but like, later
+* Return: 0, no mas loop
 */
 
 int main(void)
+
 {
 
-_putchar("Holberton\n");
+char a[9];
+
+int q;
+
+
+q = 0;
+
+a[0] = 'H';
+a[1] = 'o';
+a[2] = 'l';
+a[3] = 'b';
+a[4] = 'e';
+a[5] = 'r';
+a[6] = ('t');
+a[7] = 'o';
+a[8] = 'n';
+
+while (q <= 9)
+
+{
+
+
+_putchar(a[q]);
+
+q++;
+
+}
+
+
+_putchar('\n');
 
 return (0);
+
 }
