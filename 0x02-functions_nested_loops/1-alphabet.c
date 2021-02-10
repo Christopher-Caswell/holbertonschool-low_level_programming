b4c532e9e8806ff583c51e9e31883e48307900de
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-10 02:44:59 +0000

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,31 +3,27 @@
 /**
 * main - primary table of directions
 *
-* Return: 0, function will end upon run 
+* Return: 0, function will end upon run
 *
 */
 
 void print_alphabet(void)
 {
 
-/**  Write a function that prints the alphabet, in lower case, followed by a line */
-
+/**
+* Write a function that prints the alphabet
+* in lower case, followed by a line
+*
+* learned ascii is the called function
+* when a number is used for a character
+*/
 
 char a;
 
-
-a = ('a');
-
-
-for (a = 'a'; a <= 'z'; a++)
-
-{
+for (a = 97; a <= 122; a++)
 
 _putchar(a);
-
-
-}
-  _putchar(\n);
+_putchar(\n);
 
 return (0);
 
