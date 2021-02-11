b19996600c5dbb96d6f59a17c683b8d92bf4697f
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-11 22:47:58 +0000

    Init Repo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* _isupper - Check if an int is uppercase
+* @c: variable used in hurr
+* Return: 1 if upper, 0 if not
+*/
+
+
+int _isupper(int c)
+{
+
+if
+
+(c >= 'A' && c <= 'Z')
+
+/*
+* printf("%c: %d\n", c, _isupper(c));
+* c = 'a';
+*
+* printf("%c: %d\n", c, _isupper(c));
+* return (0);
+*/
+
+return (1);
+
+else
+
+return (0);
+
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Nested loops and some fun codes
