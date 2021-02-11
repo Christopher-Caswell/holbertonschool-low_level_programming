ab5da89c68566e25187a4ac5f5f6169ae0a1c30f
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-11 23:08:27 +0000

    Init 1-

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,34 @@
 #include "holberton.h"
 
 /**
- * 
+* _isdigit - function that checks
+* if a digit is between 0 and 9
+* @c: the variable
+* Return: 0, end after print
+*/
+
+int _isdigit(int c)
+{
+/*
+* Write a function that checks for a digit (0-9)
+*/
+
+
+char c;
+
+if (char c >= 48 && c <= 57)
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+}
