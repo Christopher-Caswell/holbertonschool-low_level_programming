8b8209d94f78f1c5771c672b74743851fd9b6611
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-12 04:57:26 +0000

    6 init

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_line - prints a line
+* @n: times to print
+* Return: 0, end
+**/
+
+void print_line(int n)
+{
+
+int x;
+
+for (x = 0; x < n; x++)
+{
+
+_putchar(95);
+}
+
+_putchar('\n');
+}
