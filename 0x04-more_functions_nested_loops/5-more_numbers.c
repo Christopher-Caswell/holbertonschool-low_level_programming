2843afc33d5c655143f8a5e88b2afa23e30756fa
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-12 04:13:20 +0000

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
 * more_numbers - prototype to print numbers 10 times
 *
-* Return: 0, end.
+* Return: 0
 */
 
 void more_numbers(void)
@@ -15,7 +15,7 @@ int y;
 for (x = 0; x < 10; x++)
 {
 
-for (y = 0; y < 15; y++)
+for (y = 0; y <= 14; y++)
 {
 
 if (y >= 10)
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -10,6 +10,6 @@ void print_numbers(void);
 int mul(int a, int b);
 void print_diagonal(int n);
 void print_most_numbers(void);
-void print_more_numbers(void);
+void more_numbers(void);
 
 #endif
