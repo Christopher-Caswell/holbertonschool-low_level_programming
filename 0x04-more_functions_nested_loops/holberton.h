582911ae5e0c9c2cdb96af32054810a7deb61c90
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-12 04:07:15 +0000

    5 init

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
+
+
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
@@ -8,6 +10,6 @@ void print_numbers(void);
 int mul(int a, int b);
 void print_diagonal(int n);
 void print_most_numbers(void);
-
+void print_more_numbers(void);
 
 #endif
