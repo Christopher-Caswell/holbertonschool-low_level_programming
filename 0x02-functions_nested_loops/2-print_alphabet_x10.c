b5e0d61f54dcd71c6bc87066b99016a61d98c340
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-13 23:09:33 +0000

    .

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* main - print the alphabet 10x
+* print_alphabet_x10 - print the alphabet 10x
 *
 * Return: 0, stop
 *
@@ -20,23 +20,23 @@ char y;
 
 y = 0;
 
+while (y < 10)
+{
 
-while (y = 0; < 10)
+x = 'a';
+
+while (x <= 'z')
 {
 
 
-while (x = "a"; <= "z")
-{
-
-
-_putchar(x);
+putchar(x);
 x++;
 }
 
-_putchar(\n);
+putchar('\n');
 y++;
 }
 
 
-return (0);
+return;
 }
