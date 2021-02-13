3f9226d2eb3e125dac3c65aeb47251fcd7b7cf86
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-13 22:41:35 +0000

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* main - primary table of directions
+* print_alphabet - ...yes
 *
 * Return: 0, function will end upon run
 *
@@ -10,7 +10,7 @@
 void print_alphabet(void)
 {
 
-/**
+/*
 * Write a function that prints the alphabet
 * in lower case, followed by a line
 *
@@ -22,9 +22,9 @@ char a;
 
 for (a = 97; a <= 122; a++)
 
-_putchar(a);
-_putchar(\n);
+putchar(a);
+putchar('\n');
 
-return (0);
+return;
 
 }
