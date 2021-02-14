7d3df05f1ed4d214c018b10e6e62441a74cdd161
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-14 03:48:15 +0000

    fizz, but also buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,50 +1,37 @@
 #include "holberton.h"
-
 /**
 * main - do the thing
 *
 * Return: , fin
 */
-
 int main(void)
 {
-
 char x;
-x = 1;
+x = 0;
 
-
-while (x <= 100)
-x++;
+while (x < 100)
 {
-
+x++;
 if (x % 3 == 0 && x % 5 == 0)
 {
-
 printf("fizzbuzz");
 putchar(' ');
 }
-
 if (x % 3 == 0)
 {
-
 printf("fizz");
 putchar(' ');
 }
-
 else if (x % 5 == 0)
 {
-
 printf("buzz");
 putchar(' ');
 }
-
 else
 {
-
-putchar(x);
+printf("%d", x);
 putchar(' ');
-
-
 }
-
+}
 return (0);
+}
