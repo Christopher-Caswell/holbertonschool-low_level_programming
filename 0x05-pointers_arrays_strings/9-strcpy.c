954eec1dd0aac23edb72186fc94776155693b4dc
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-18 06:03:19 +0000

    9 update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,46 @@
 #include "holberton.h"
 
 /**
+ * *_strcpy - print string
+ * @dest: jargon
+ * @src: jargon
  *
- *
- *
+ * Return: nada
  */
-
 char *_strcpy(char *dest, char *src)
 {
-  char x;
 
-  for (x = 0; x <= len(src); x++)
-    {
-      _strcopy(src, *dest);
-    }
+int x, y;
 
+x = _strlen(src);
+
+for (y = 0; y <= x; y++)
+{
+
+dest[y] = src[y];
+}
+
+return (dest);
+}
+
+/**
+ * _strlen - return the length of the string
+ *@s: the length of the string
+ *
+ * Return: the length(x)
+ */
+
+int _strlen(char *s)
+{
+
+int x;
+x = 0;
+
+while (*(s + x) != '\0')
+{
+
+x++;
+}
+
+return (x);
 }
