9ec527da051855b23d4f339f4243abe0ccee9338
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-19 20:41:29 +0000

    6 init

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * cap_string - cap the first letter of every word
+ * @django: string
+ * Return: djangarang
+ */
+
+char *cap_string(char *django)
+{
+int bangarang;
+
+for (bangarang = 0; django[bangarang] != 00; bangarang++)
+{
+
+if ((django[bangarang] == ('\n')) || (django[bangarang] == (' ')) ||
+(django[bangarang] == 9) || 
+(django[bangarang] == 44) || (django[bangarang] == 59) ||
+(django[bangarang] == 46) || (django[bangarang] == 33) ||
+(django[bangarang] == 63) || (django[bangarang] == 34) ||
+(django[bangarang] == 40) || (django[bangarang] == 41) ||
+(django[bangarang] == 123) || (django[bangarang] == 125))
+{
+
+if (django[bangarang + 1] >= 97 && django[bangarang + 1] <= 122)
+{
+
+django[bangarang + 1] = django[bangarang + 1] - 32;
+
+
+}
+}
+}
+return (django);
+}
