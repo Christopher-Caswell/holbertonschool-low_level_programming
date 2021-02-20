2a9c907e9dd9ad2d236a81175aa41b06b84c7dab
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-20 06:01:17 +0000

    8 init

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * leet - 1337 y0
+ * @array: 743 4rr4/
+ * Return: 743 1|\|f0
+ */
+
+char *rot13(char *array)
+{
+int ex, y;
+
+
+char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *replace = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (ex = 0; array[ex] != 00; ex++)
+{
+
+for (y = 0; y < 52; y++)
+{
+
+if (array[ex] == original[y])
+{
+
+array[ex] = replace[y];
+break;
+}
+}
+}
+return (array);
+}
