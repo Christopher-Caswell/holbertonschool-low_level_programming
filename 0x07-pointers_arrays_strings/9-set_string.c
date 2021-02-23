1c9fac710a398cc5f280cb2bb3308a5a648df4c6
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-23 03:58:11 +0000

    9 init

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - set the string to a character
+ * @s: the string
+ * @to: the to
+ * @s: because Betty told me to
+ * Return: why?
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
