c8393a2020d72e48cda3fa51817cdcdbdad85bd7
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-25 05:09:57 +0000

    5 init

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - defining the rootable
+ * @n: the rooter
+ * Return: nootnoot to the power of n
+ */
+
+int _sqrt_recursion(int n)
+{
+
+return (nootnoot(0, n));
+}
+
+
+/**
+ * nootnoot - the identifier for x^y
+ * @x: the pass
+ * @y: the volume
+ * Return: the variable passed into _sqrt_recursion
+ */
+
+int nootnoot(int x, int n)
+{
+
+int y = x * x;
+
+if (y > n)
+{
+
+return (-1);
+}
+
+if (x * x == n)
+{
+
+return (x);
+}
+
+x++;
+return (nootnoot(x, n));
+}
