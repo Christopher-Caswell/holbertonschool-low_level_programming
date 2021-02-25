5ca99d9f8211297383e8253070a3215bdd29e2bd
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-25 06:13:47 +0000

    attempt at 100

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -11,5 +11,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int nootnoot(int x, int y);
+int check1(int x, int y, char *s1, char *s2);
+int check2(int v, char *s2);
+int wildcmp(char *s1, char *s2);
 
 #endif
