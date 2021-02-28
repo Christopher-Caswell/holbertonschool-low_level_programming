ec2ba309dc8a14b91cf27dedf7605cf09f7789a2
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-02-28 22:49:09 +0000

    2 reup

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * main - function generic
+ * @argc: voided
+ * @argv: strings, yo
+ * Return: strings, yo
  */
 
 int main(int argc, char *argv[])
 {
+
+(void)argc;
 
 int x;
 
