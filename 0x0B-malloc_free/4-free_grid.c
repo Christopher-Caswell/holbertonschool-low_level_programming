8fa82f77642a1ac45f5e005cfcb59e5d9efe3599
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-03-03 05:36:06 +0000

    4 init

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * free_grid - do what it says
+ * @grid: to be freed
+ * @height: to be void
+ * Return: free and clear
+ */
+
+void free_grid(int **grid, int height)
+{
+
+(free)grid;
+(void)height;
+
+return;
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,6 +2,7 @@
 #define WHAM
 #include <stdlib.h>
 
+void free_grid(int **grid, int height);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
