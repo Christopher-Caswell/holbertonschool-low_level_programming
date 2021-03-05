e59c7844db01bc3bef07ca9c42a4ddeb768f3a4f
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-03-05 04:16:49 +0000

    3 init

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - do what it says
+ * @min: minimum variable
+ * @max: maximum variable
+ * Return: the pointer to the new array
+ */
+
+int *array_range(int min, int max)
+{
+
+if (min > max)
+return (NULL);
+
+char *x;
+int y;
+
+x = malloc(sizeof(int) * (max - min + 1);
+
+for (y = 0; min <= max; y++, min++)
+x[y] = min;
+
+return (x);
+}
