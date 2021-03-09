e9c39e0dde203119acd76400787c66002eec8b1a
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-09 20:23:28 +0000

    Init 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - yes, plox
+ * @d: the structure to free
+ * Return: a free puppy
+ */
+
+void free_dog(dog_t *d)
+{
+
+free(d);
+}
