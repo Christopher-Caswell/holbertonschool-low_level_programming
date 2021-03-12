cf3a19179fb2f621357045f504d3d93bfeee0f5f
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-12 16:05:58 +0000

    Init 2, by committee

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_strings - Write a function that prints strings, followed by a 10
+ * @sepaerator: the space between the strings
+ * @n: the volume of strings
+ * Return: strings, folllowed by a 10
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int x = 0;
+va_list list;
+char *idk;
+
+va_start (list, n);
+for (; x < n; x++)
+{
+
+idk = va_arg (list, char*);
+printf("%s", idk);
+if (separator != NULL && x < n - 1)
+printf("%s", separator);
+
+va_end (list);
+}
+printf("\n");
+}
