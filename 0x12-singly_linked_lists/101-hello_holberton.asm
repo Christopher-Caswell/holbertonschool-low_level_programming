dc68f67ce8e7915e8cded8dea2bc7f06e9bfd28f
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-19 00:08:49 +0000

    Minor Assembly tweaks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,13 @@
     section .data
-	fmt     db "%s", 0
-	msg1    db "Hello, Holberton",0
+	fmt     db "%s", 10, 0
+	msg1    db "Hello, Holberton", 0
 
 	    section .text
 	    extern printf
 	    global _start
 
-_start:
-	    mov  edx, msg1
-	    mov  esi, 1
+_start:	
+	    mov  esi, msg1
 	    mov  edi, fmt
 	    mov  eax, 0
 	    call printf
