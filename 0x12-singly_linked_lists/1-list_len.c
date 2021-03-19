ec5ae4ac98d0be638084c8db21c56ea701b6d5c9
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-19 01:05:29 +0000

    Innit 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * list_len - the arrays inside the list
  * @h: the struct
@@ -12,5 +12,5 @@ size_t list_len(const list_t *h)
 if (h == NULL)
 return (0);
 else
-return (1 + h->len(h->next));
+return (1 + list_len(h->next));
 }
