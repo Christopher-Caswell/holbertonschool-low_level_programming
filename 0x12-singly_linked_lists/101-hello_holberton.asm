323695c96b4a11c384add7a381d4974689d65f8c
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-19 04:55:19 +0000

    reverse engineered it 2.0

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -9,7 +9,8 @@ main:
 	        movq    %rsp, %rbp
 	        movl    $.LC0, %edi
 	        popq    %rbp
-	        ret
+	call printf
+	ret
 	        .size   main, .-main
 	        .ident  "GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
 	        .section        .note.GNU-stack,"",@progbits
