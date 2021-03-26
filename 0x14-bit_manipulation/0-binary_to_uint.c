e4f08ee9c7b945ff6963a7608ad30f97b3320e46
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-03-26 01:49:20 +0000

    New mode for 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,66 +1,35 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - turn bin into dec
- * @b: the variable fed from "main.c"
- *Return: an unsigned int
+ * binary_to_uint - does what it says
+ * @b: the binary string
+ * Return: a decimal from binary
  */
 
 unsigned int binary_to_uint(const char *b)
 {
 
-int x = 0;
+unsigned int home;
 
-if (b == NULL)
+if (!b)
 return (0);
 
-for (; b[x] <= 00; x++)
+for (home = 0; *b; b++)
 {
 
-if (x != 0 && x!= 1)
+if (*b != 48 && *b != 49)
 return (0);
+
+if (*b == '1')
+home = (home << 1) | 1;
+
+else if (*b == '0')
+home <<= 1;
 
 else
-return (rev_bin(b));
-}
-
-/**
- * rev_bin - helper to run the numbers
- * @b: the same pointer as above
- * Return: a binary conversion of the string
- */
-
-int rev_bin(*b)
-{
-
-int x = 0;
-int y = 0;
-
-for(; b[x] == b[str_len]; y++, x--)
-{
-a = x + (b[x^y]);
-
-for (; z <= y; z++)
-{
-
+break;
 
 }
-}
-return (a);
-}
 
-/**
- * _strlen - return the length of the string
- *@s: the length of the string
- * Return: the length(x)
- */
-int _strlen(char *s)
-{
-int x;
-x = 0;
-while (*(s + x) != '\0')
-{
-x++;
-}
-return (x);
+return (home);
 }
