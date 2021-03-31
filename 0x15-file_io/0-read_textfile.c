8ac4ea25ab34a143d0ee2da4d321d3f648d9c0cf
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-03-31 01:28:57 +0000

    Init base files, 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - read a book
+ * @filename: the name of the book
+ * @letters: the pages of the book
+ * Return: the book to the library. . . or not if the book is empty
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+char *buffer;
+ssize_t reads = 0, writes, count;
+
+if (!filename)
+{
+
+return (0);
+}
+
+buffer = malloc(sizeof(buffer) * letters);
+
+if (!buffer)
+{
+
+return (0);
+}
+
+count = open(filename, O_RDONLY);
+
+if (count == -1)
+{
+
+free(buffer);
+return (0);
+}
+
+reads = read(count, buffer, letters);
+
+if (reads == -1)
+{
+
+free(buffer);
+return (0);
+}
+
+writes = write(1, buffer, reads);
+
+if (writes == -1)
+{
+
+reads = 0;
+}
+
+free(buffer);
+close(count);
+return (reads);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This folder is reserved for study in the holberton Curriculum in March of 2021 regarding input/output system calls
