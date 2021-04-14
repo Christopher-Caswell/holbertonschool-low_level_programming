26e35853d07eb8f1017949fc47d7c7b602bb80e2
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-14 14:19:22 +0000

    Betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,23 +4,17 @@
 * print_alphabet_x10 - print the alphabet 10x
 *
 * Return: 0, stop
-*
 */
 
 void print_alphabet_x10(void)
 {
-
-/**
-* write a function that prints the alphabet -10x,
-*in lowercase, followed by a new line
-*/
 
 char x;
 char y;
 
 y = 0;
 
-while  (y < 10)
+while (y < 10)
 {
 
 x = 'a';
@@ -37,7 +31,6 @@ _putchar('\n');
 
 y++;
 }
-
 
 return;
 }
