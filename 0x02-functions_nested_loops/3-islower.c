3d47b8e20d45b20cd4b81282beeb082bcfe28a39
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-14 14:23:31 +0000

    Betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,47 +2,20 @@
 
 /**
 * main - checks for lowercase character
-* Return: 0, one run and done Return: 1, run again
+* @c: int passed though
+* Return: 0 if incorrect
 */
 
 int _islower(int c)
 {
 /**write a function that checks for lowercase character*/
-
-
 int _islower(int c)
 
-
-{
-
-
 if (c >= 'a' && c <= 'z')
-
-
 {
-
 
 return (1);
-
-
 }
 
-
-else
-
-
-{
-
-
 return (0);
-
-
-}
-
-
-
-
-return (0);
-
-
 }
