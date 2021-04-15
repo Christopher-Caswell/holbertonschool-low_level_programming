fbe95275fd593505ab275d505144f3993bdfe132
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-15 23:52:33 +0000

    Init 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print zero through 9
+ * Return: nothing, after print
+ */
+
+int main(void)
+{
+int x;
+
+for (x = 48; x <= 57; x++)
+{
+
+putchar(x);
+}
+putchar(10);
+return (0);
+}
