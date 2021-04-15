f8a24804895dd674ea857fb201a7e098f85262d9
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-15 23:42:22 +0000

    Init 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - begin the function
+ * Return: 0, no reset on end
+ */
+
+int main(void)
+{
+char x;
+
+for (x = 122; x >= 97; x--)
+{
+
+putchar(x);
+}
+
+putchar(10);
+return (0);
+}
