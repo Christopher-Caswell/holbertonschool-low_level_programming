033bbc57fd1cfd3d2f7c5bb9e877f62891fef51f
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-02 18:37:03 +0000

    Init

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+> This repository is to be utilized for the purposes of Holberton Curriculum in regard to making a dynamic library
---
+++ 0x18-dynamic_libraries/libwat.c
@@ -0,0 +1,115 @@
+int _putchar(char c)
+{
+(void)c;
+return 0;
+}
+int _islower(int c)
+{
+(void)c;
+return 0;
+}
+int _isalpha(int c)
+{
+  (void)c;
+return 0;
+}
+int _abs(int n)
+{
+  (void)n;
+return 0;
+}
+int _isupper(int c)
+{
+  (void)c;
+return 0;
+}
+int _isdigit(int c)
+{
+  (void)c;
+return;
+}
+int _strlen(char *s)
+{
+  (void)s;
+return;
+}
+void _puts(char *s)
+{
+  (void)s;
+return;
+}
+char *_strcpy(char *dest, char *src)
+{
+  (void)dest;
+  (void)src;
+return;
+}
+int _atoi(char *s)
+{
+  (void)s;
+return;
+}
+char *_strcat(char *dest, char *src)
+{
+  (void)dest;
+  (void)src;
+return;
+}
+char *_strncat(char *dest, char *src, int n)
+{
+  (void)src;
+  (void)dest;
+  (void)n;
+return;
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+  (void)dest;
+  (void)src;
+  (void)n;
+return;
+}
+int _strcmp(char *s1, char *s2)
+{
+  (void)s1;
+  (void)s2;
+return;
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+  (void)n;
+  (void)s;
+  (void)b;
+return;
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  (void)dest;
+  (void)src;
+  (void)n;
+return;
+}
+char *_strchr(char *s, char c)
+{
+  (void)s;
+  (void)c;
+return;
+}
+unsigned int _strspn(char *s, char *accept)
+{
+  (void)s;
+  (void)accept;
+return;
+}
+char *_strpbrk(char *s, char *accept)
+{
+  (void)s;
+  (void)accept;
+return;
+}
+char *_strstr(char *haystack, char *needle)
+{
+  (void)haystack;
+(void)needle;
+return;
+}
