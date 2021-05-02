7804910959d3907a2fcfcd52c1b08ff984801f03
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-02 19:20:07 +0000

    uploaded to split

--- 0x18-dynamic_libraries/libwat.c
+++ 0x18-dynamic_libraries/libwat.c
@@ -1,115 +1,252 @@
+/**
+ * _putchar - void
+ * @c: void
+ * Return: void
+ */
 int _putchar(char c)
 {
 (void)c;
-return 0;
+return;
 }
+
+/**
+ * _islower - void
+ * @c: void
+ * Return: void
+ */
 int _islower(int c)
 {
 (void)c;
-return 0;
+return;
 }
+
+/**
+ * _isalpha - void
+ * @c: void
+ * Return: void
+ */
 int _isalpha(int c)
 {
-  (void)c;
-return 0;
+(void)c;
+return;
 }
+
+/**
+ * _abs - void
+ * @n: void
+ * Return: void
+ */
 int _abs(int n)
 {
-  (void)n;
-return 0;
+(void)n;
+return;
 }
+
+/**
+ * _isupper - void
+ * @c: void
+ * Return: void
+ */
 int _isupper(int c)
 {
-  (void)c;
-return 0;
+(void)c;
+return;
 }
+thats 5
+
+/**
+ * _isdigit - void
+ * @c: void
+ * Return: void
+ */
 int _isdigit(int c)
 {
-  (void)c;
+(void)c;
 return;
 }
+
+/**
+ * _strlen - void
+ * @s: void
+ * Return: void
+ */
 int _strlen(char *s)
 {
-  (void)s;
+(void)s;
 return;
 }
+
+/**
+ * _puts - void
+ * @s: void
+ * Return: void
+ */
 void _puts(char *s)
 {
-  (void)s;
+(void)s;
 return;
 }
+
+/**
+ * _strcpy - void
+ * @dest: void
+ * @src: void
+ * Return: void
+ */
 char *_strcpy(char *dest, char *src)
 {
-  (void)dest;
-  (void)src;
+(void)dest;
+(void)src;
 return;
 }
+
+/**
+ * _atoi - void
+ * @s: void
+ * Return: void
+ */
 int _atoi(char *s)
 {
-  (void)s;
+(void)s;
 return;
 }
+thats 5
+
+/**
+ * _strcat - void
+ * @dest: void
+ * @src: void
+ * Return: void
+ */
 char *_strcat(char *dest, char *src)
 {
-  (void)dest;
-  (void)src;
+(void)dest;
+(void)src;
 return;
 }
+
+/**
+ * _strncat - void
+ * @dest: void
+ * @src: void
+ * @n: void
+ * Return: void
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-  (void)src;
-  (void)dest;
-  (void)n;
+(void)src;
+(void)dest;
+(void)n;
 return;
 }
+
+/**
+ * _strncpy - void
+ * @dest: void
+ * @src: void
+ * @n: void
+ * Return: void
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-  (void)dest;
-  (void)src;
-  (void)n;
+(void)dest;
+(void)src;
+(void)n;
 return;
 }
+
+/**
+ * _strcmp - void
+ * @s1: void
+ * @s2: void
+ * Return: void
+ */
 int _strcmp(char *s1, char *s2)
 {
-  (void)s1;
-  (void)s2;
+(void)s1;
+(void)s2;
 return;
 }
+
+/**
+ * _memset - void
+ * @s: void
+ * @b: void
+ * @n: void
+ * Return: void
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-  (void)n;
-  (void)s;
-  (void)b;
+(void)n;
+(void)s;
+(void)b;
 return;
 }
+thats 5
+
+/**
+ * _memcpy - void
+ * @dest: void
+ * @src: void
+ * @n: void
+ * Return: void
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  (void)dest;
-  (void)src;
-  (void)n;
+(void)dest;
+(void)src;
+(void)n;
 return;
 }
+
+/**
+ * _strchr - void
+ * @c: void
+ * @s: void
+ * Return: void
+ */
 char *_strchr(char *s, char c)
 {
-  (void)s;
-  (void)c;
+(void)s;
+(void)c;
 return;
 }
+
+/**
+ * _strspn - void
+ * @s: void
+ * @accept: void
+ * Return: void
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-  (void)s;
-  (void)accept;
+(void)s;
+(void)accept;
 return;
 }
+
+/**
+ * _strpbrk - void
+ * @s: void
+ * @accept: void
+ * Return: void
+ */
 char *_strpbrk(char *s, char *accept)
 {
-  (void)s;
-  (void)accept;
+(void)s;
+(void)accept;
 return;
 }
+
+/**
+ * _strstr - void
+ * @haystack: void
+ * @needle: void
+ * Return: void
+ */
 char *_strstr(char *haystack, char *needle)
 {
-  (void)haystack;
+(void)haystack;
 (void)needle;
 return;
 }
