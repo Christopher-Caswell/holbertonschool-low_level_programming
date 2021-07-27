769f8e01995e40d6ef307a51702215000a7b27e6
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-27 14:02:57 -0500

    Betty linter is a fiend.

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -8,14 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *coffee = NULL;
+hash_table_t *coffee = NULL;
 
-	coffee = malloc(sizeof(hash_table_t));
-	if (!coffee)
-		return (NULL);
-	coffee->array = malloc(sizeof(hash_node_t *) * size);
-	coffee->size = size;
-	if (!coffee->array)
-		return (NULL);
-	return (coffee);
+coffee = malloc(sizeof(hash_table_t));
+
+if (!coffee)
+
+return (NULL);
+
+coffee->array = malloc(sizeof(hash_node_t *) * size);
+coffee->size = size;
+
+if (!coffee->array)
+return (NULL);
+
+return (coffee);
 }
