e92bde546d08b5cd62830e182c5effdd3e38ffc7
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-28 14:42:57 -0500

    try again, checker

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -1,35 +1,62 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_set - Hash table set
+* hash_table_set - Adds an element to the hash table
 *  
 * @key: Key
 * @value: Value
+* @hash_table_t: Hash table
 * 
 * Return: 1 if successful, 0 if not
 */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    int i;
-    int hash_value;
-    hash_entry_t *entry;
 
-    hash_value = hash_string(key);
-    i = hash_value % ht->size;
-    entry = ht->table[i];
-    while (entry != NULL) {
-        if (strcmp(entry->key, key) == 0) {
-            entry->value = value;
-            return 1;
-        }
-        entry = entry->next;
+    int x;
+    hash_node_t *newguy = NULL, *temp = NULL;
+
+    if (ht == NULL || key == NULL || value == NULL)
+    {
+        return 0;
     }
-    entry = malloc(sizeof(hash_entry_t));
-    entry->key = key;
-    entry->value = value;
-    entry->next = ht->table[i];
-    ht->table[i] = entry;
+
+    newguy = (hash_node_t *)malloc(sizeof(hash_node_t));
+    if (newguy == NULL)
+    {
+        return 0;
+    }
+
+    newguy->key = (char *)malloc(strlen(key) + 1);
+    if (newguy->key == NULL)
+    {
+        free(newguy);
+        return 0;
+    }
+    strcpy(newguy->key, key);
+    newguy->value = (char *)malloc(strlen(value) + 1);
+    if (newguy->value == NULL)
+    {
+        free(newguy->key);
+        free(newguy);
+        return 0;
+    }
+    strcpy(newguy->value, value);
+    newguy->next = NULL;
+
+    if (ht->table[x] == NULL || ht->table[x]->next == NULL)
+    {
+        ht->table[x] = newguy;
+    }
+    else
+    {
+        temp = ht->table[x];
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newguy;
+    }
     ht->count++;
     return 1;
 }
