8c9555d6c0c47f69636b7df781c9845fdd7e193a
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-27 22:59:07 -0500

    run

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,10 +1,16 @@
 #include "hash_tables.h"
 
+/**
+ * djb2 hash function
+ * @param key
+ * Return: hash value
+*/
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash = 5381;
-    int c;
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    return hash;
+unsigned long int hash = 5381;
+int c;
+while ((c = *str++))
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+return (hash);
 }
