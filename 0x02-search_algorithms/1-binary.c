e0b8c0579bcc7d146e73839b3b0c5dad0a4c8b25
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-09-20 00:41:00 -0500

    Logical dissonance corrected

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
         else if (array[mid] < value)
             printf(", %d", array[mid]);
             start = mid + 1;
-        else
+        else if (array[mid] > value)
             printf(", %d", array[mid]);
             end = mid - 1;
     }
