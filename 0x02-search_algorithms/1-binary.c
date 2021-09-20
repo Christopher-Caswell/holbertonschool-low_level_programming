38049cb37b450bc1f4076584f83c3e966c1450e8
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-09-20 01:31:58 -0500

    Linter update

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -5,38 +5,45 @@
  * @array: pointer to the first element of the array
  * @size: size of the array
  * @value: value to search for
- * 
+ *
  * Return: index of the value if found, -1 otherwise
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t start = 0, end = size - 1, mid, x;
+size_t start = 0, end = size - 1, mid, x;
 
-    if (array == NULL)
-        return (-1);
+if (array == NULL)
+return (-1);
 
-    while (start <= end)
-    {
-        mid = (start + end) / 2;
-        printf("Searching in array: ");
-        for (x = start; x < end; x++)
-            printf("%d, ", array[x]);
-        printf("%d\n", array[x]);
-        if (array[mid] == value)
-            return (mid);
-        if (array[mid] < value)
-        {
-            start = mid + 1;
-            if (start > end)
-                return (-1);
-        }
-        if (array[mid] > value)
-        {
-            end = mid - 1;
-            if (end < start)
-                return (-1);
-        }
-    }
-    return (-1);
+while (start <= end)
+{
+
+mid = (start + end) / 2;
+printf("Searching in array: ");
+
+for (x = start; x < end; x++)
+printf("%d, ", array[x]);
+printf("%d\n", array[x]);
+
+if (array[mid] == value)
+return (mid);
+
+if (array[mid] < value)
+{
+start = mid + 1;
+
+if (start > end)
+return (-1);
+}
+
+if (array[mid] > value)
+{
+end = mid - 1;
+
+if (end < start)
+return (-1);
+}
+}
+return (-1);
 }
