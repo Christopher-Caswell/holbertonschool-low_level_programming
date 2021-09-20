273fc9eac979c7afea241afd52b09bf7786a4070
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-09-20 01:05:03 -0500

    Binary printing fixed. Logic.

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -11,15 +11,18 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t start = 0, end = size - 1, mid;
+    size_t start = 0, end = size - 1, mid, x;
 
     if (array == NULL)
         return (-1);
 
     while (start <= end)
     {
-        printf("Searching in array: ");
         mid = (start + end) / 2;
+        printf("Searching in array: ");
+        for (x = start; x < end; x++)
+            printf("%d, ", array[x]);
+        printf("%d\n", array[x]);
         if (array[mid] == value)
             return (mid);
         if (array[mid] < value)
