a5913b763645bb83befcc3ad2b5a10de55941b4c
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-09-20 00:38:30 -0500

    Left justified to appease linter

--- 0x02-search_algorithms/0-linear.c
+++ 0x02-search_algorithms/0-linear.c
@@ -5,23 +5,24 @@
  * @array: array of integers, assigned via pointer
  * @size: size of array
  * @value: value for whom we seek
- * 
+ *
  * Return: index of value or -1 if not found
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int x;
-    
-    if (array == NULL)
-        return (-1);
+unsigned int x;  
 
-    for (x = 0; x < size; x++)
-    {
-        printf("Value checked array[%d] = [%d]\n", x, array[x]);
-        if (array[x] == value)
-            return (x);
-    }
+if (array == NULL)
+return (-1);
 
-    return (-1);
+for (x = 0; x < size; x++)
+{
+
+printf("Value checked array[%d] = [%d]\n", x, array[x]);
+if (array[x] == value)
+return (x);
+}
+
+return (-1);
 }
